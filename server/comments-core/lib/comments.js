0623b72a4cc57de719115000ff30b5e673887902
Filippo De Santis <filippo.desantis@gmail.com>
2018-05-23 14:19:07 +0200

    Use node 10.1.0

--- server/comments-core/lib/comments.js
+++ server/comments-core/lib/comments.js
@@ -3,30 +3,20 @@
 const _ = require('lodash')
 const SQL = require('@nearform/sql')
 
-let db
+module.exports = function build (db) {
+  return { add }
 
-function add (data, done) {
-  const { reference, comment, author } = data
-  db.query(SQL`
-    INSERT INTO
-      comment (reference, comment, author)
-    VALUES (${reference}, ${comment}, ${author})
-    RETURNING *
-  `, (err, res) => {
-    if (err) return done(err)
+  function add (data, done) {
+    const { reference, comment, author } = data
+    db.query(SQL`
+      INSERT INTO
+        comment (reference, comment, author)
+      VALUES (${reference}, ${comment}, ${author})
+      RETURNING *
+    `, (err, res) => {
+      if (err) return done(err)
 
-    done(null, _.get(res, 'rows.0'))
-  })
+      done(null, _.get(res, 'rows.0'))
+    })
+  }
 }
-
-const comments = {
-  add
-}
-
-function initModule (connection) {
-  db = connection
-
-  return comments
-}
-
-module.exports = initModule
