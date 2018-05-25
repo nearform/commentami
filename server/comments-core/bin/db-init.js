637437fd323bec90b7107b3e5801af907246303b
Filippo De Santis <filippo.desantis@gmail.com>
2018-05-25 09:55:09 +0200

    Add command to initialize db and convert db-init to async/await

--- server/comments-core/bin/db-init.js
+++ server/comments-core/bin/db-init.js
@@ -2,27 +2,28 @@
 'use strict'
 
 const { assign, cloneDeep } = require('lodash')
-const async = require('async')
 const conf = require('../config')
 const { initClient, killOutstandingConnections, dropDb, createDb } = require('../lib/db')
 
 const initDbConfig = assign({}, cloneDeep(conf.pg), { database: 'postgres' })
 const client = initClient(initDbConfig)
 
-async.series(
-  [
-    (next) => client.connect(next),
-    (next) => { !conf.isProd ? killOutstandingConnections(client, conf.pg.database, next) : next() },
-    (next) => dropDb(client, conf.pg.database, next),
-    (next) => createDb(client, conf.pg.database, next),
-    (next) => client.end(next)
-  ],
-  (err) => {
-    if (err) {
-      console.error(err) // eslint-disable-line no-console
-      process.exit(1)
-    }
-
-    console.info('Db init: done') // eslint-disable-line no-console
+const result = (async function () {
+  await client.connect()
+  if (!conf.isProd) {
+    await killOutstandingConnections(client, conf.pg.database)
   }
-)
+
+  await dropDb(client, conf.pg.database)
+  await createDb(client, conf.pg.database)
+  await client.end()
+})()
+
+result
+  .then(() => {
+    console.info('Db init: done') // eslint-disable-line no-console
+  })
+  .catch((err) => {
+    console.error(err) // eslint-disable-line no-console
+    process.exit(1)
+  })
