2a116a56ce2ff0e1dd00985581a3241a4985178d
Shogun <shogun@cowtech.it>
2018-05-25 12:17:36 +0200

    fix(ci): Enforce JSX extensions.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['./node_modules/@cowtech/eslint-config/react.js'],
+  rules: [
+    "react/jsx-filename-extension": [2, { "extensions": [".jsx"] }]
+  ]
   globals: {
     describe: true,
     test: true,
