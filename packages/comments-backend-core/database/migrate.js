7f104220a0273f09280997a35287611a564212c3
Shogun <shogun@cowtech.it>
2018-05-25 17:08:10 +0200

    fix: Fix migration script.

--- packages/comments-backend-core/database/migrate.js
+++ packages/comments-backend-core/database/migrate.js
@@ -6,11 +6,11 @@ const path = require('path')
 
 async function run() {
   const version = process.argv[2]
-  const { host, port, database, user: username, password } = config.pg
+  const { host, port, database, username: user, password } = config.pg
   const migrationDirectory = path.join(__dirname, '/migrations')
   if (!version) throw new Error('Please provide the version to migrate to as first command line argument')
 
-  const postgrator = new Postgrator({ driver: 'pg', migrationDirectory, schemaTable: 'schemaversion', host, port, database, username, password })
+  const postgrator = new Postgrator({ driver: 'pg', migrationDirectory, schemaTable: 'schemaversion', host, port, database, user, password })
 
   await postgrator.migrate(version)
   console.log(`\x1b[32m\u2714 Database \x1b[1m${database}\x1b[22m migrated successfully to version ${version}!\x1b[0m`)
