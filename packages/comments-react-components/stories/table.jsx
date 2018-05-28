5951782bd516a0c3acfca8d608d5a4222ea53294
Davide Fiorello <davide@codeflyer.com>
2018-05-28 15:46:34 +0200

    feat(api-integration): updated table example story

--- packages/comments-react-components/stories/table.jsx
+++ packages/comments-react-components/stories/table.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import sampleData from './data/sample'
+import { CommentsInMemoryService } from '../src/services/CommentsInMemoryService'
 import { CommentableProvider } from '../src/components/Commentable/Provider'
 import { CommentsBlock } from '../src/components/Commentable/CommentsBlock'
 import { NewCommentPopUp } from '../src/components/Commentable/NewCommentPopUp'
 
 import { Table } from './components/table'
 
+const commentService = CommentsInMemoryService()
+
 storiesOf('Commentable/Table', module).add('Sample', () => (
-  <CommentableProvider>
+  <CommentableProvider service={commentService}>
     <NewCommentPopUp />
     <div style={{ marginLeft: '20px' }}>
       <Table
