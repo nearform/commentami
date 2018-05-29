550b2bb141542f85a7dd503620abfc7ff3d911d2
Shogun <shogun@cowtech.it>
2018-05-29 14:48:08 +0200

    chore: Add an barebone example that reflects README.md

---
+++ packages/comments-react-components/stories/readme.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import { CommentableProvider } from '../src/components/CommentableProvider'
+import { CommentableBlock } from '../src/components/CommentableBlock'
+
+import { CommentsInMemoryService } from './helpers/CommentsInMemoryService'
+import { EventsManager } from './components/EventsManager'
+
+const commentService = CommentsInMemoryService()
+
+storiesOf('Commentable/Readme', module).add('Sample', () => (
+  <CommentableProvider resource="main" service={commentService} eventsManagerComponent={EventsManager}>
+    <div style={{ marginLeft: '30px' }}>
+      <CommentableBlock referenceId="comm-1">
+        <h1>Text Title 1</h1>
+      </CommentableBlock>
+      <CommentableBlock referenceId="comm-2">
+        <p>Paragraphs are separated by a blank line.</p>
+      </CommentableBlock>
+    </div>
+  </CommentableProvider>
+))
