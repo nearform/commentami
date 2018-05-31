4804050eccb9f625c09654cf8b7138df9c14ec36
Filippo De Santis <filippo.desantis@gmail.com>
2018-05-31 15:48:57 +0200

    Use multines for multinode websocket notifications

--- packages/comments-backend-hapi-plugin/lib/index.js
+++ packages/comments-backend-hapi-plugin/lib/index.js
@@ -1,21 +1,9 @@
 'use strict'
 
-const redis = require('mqemitter-redis')
 const Nes = require('nes')
+const Multines = require('multines')
 const { buildCommentsService, buildPool, config } = require('@nearform/comments-backend-core')
 const { notifyComment } = require('./subscriptions')
-
-const kDeliver = Symbol.for('deliver')
-
-function buildDeliver (socket, topic) {
-  return async function deliver (message, done) {
-    if (topic === message.topic) {
-      await socket.publish(topic, message.body)
-    } else {
-      await socket.publish(topic, message)
-    }
-  }
-}
 
 const commentsHapiPlugin = {
   name: 'comments-hapi-plugin',
@@ -29,73 +17,18 @@ const commentsHapiPlugin = {
     server.decorate('request', 'commentsService', commentsService)
 
     if (options.disableWebsocket !== true) {
-      await server.register(Nes)
+      let { redis } = options
+      if (!redis) {
+        redis = {}
+      }
 
-      const mq = redis(options.redis)
-
-      server.decorate('server', 'subscriptionFar', (path, options) => {
-        options = options || {}
-
-        const wrapSubscribe = options.onSubscribe || (async (socket, path, params) => null)
-        const wrapUnsubscribe = options.onUnsubscribe || (async (socket, path, params) => null)
-
-        options.onSubscribe = async (socket, path, params) => {
-          const deliverMap = socket[kDeliver] || {}
-          socket[kDeliver] = deliverMap
-
-          if (!deliverMap[path]) {
-            deliverMap[path] = buildDeliver(socket, path)
-          }
-
-          await wrapSubscribe(socket, path, params)
-          await new Promise((resolve, reject) => {
-            mq.on(path, deliverMap[path], (err) => {
-              if (err) return reject(err)
-              resolve()
-            })
-          })
-        }
-
-        options.onUnsubscribe = async (socket, path, params) => {
-          await wrapUnsubscribe(socket, path, params)
-
-          const deliverMap = socket[kDeliver] || {}
-          socket[kDeliver] = deliverMap
-
-          if (!deliverMap[path]) {
-            return
-          }
-
-          await new Promise((resolve, reject) => {
-            mq.removeListener(path, deliverMap[path], (err) => {
-              if (err) return reject(err)
-              resolve()
-            })
-          })
-        }
-
-        server.subscription(path, options)
-      })
-
-      server.decorate('server', 'publishFar', async (path, body) => {
-        options = options || {}
-
-        await new Promise((resolve, reject) => {
-          mq.emit({ topic: path, body }, (err) => {
-            if (err) return reject(err)
-            resolve()
-          })
-        })
-      })
-
-      server.ext('onPostStop', async () => {
-        await new Promise((resolve, reject) => {
-          mq.close((err) => {
-            if (err) return reject(err)
-            resolve()
-          })
-        })
-      })
+      await server.register([Nes, {
+        plugin: {
+          name: 'multines',
+          register: Multines.register
+        },
+        options: { type: 'redis', ...redis }
+      }])
 
       server.subscriptionFar('/resources/{resource*}')
       server.subscriptionFar('/resources-reference/{reference}/{resource*}')
---
+++ packages/comments-backend-hapi-plugin/test/websocket-multihost-subscriptions.test.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const Nes = require('nes')
+const { expect } = require('code')
+
+const Lab = require('lab')
+module.exports.lab = Lab.script()
+const { describe, it: test, before, after } = module.exports.lab
+
+const { resetDb } = require('../../comments-backend-core/test/utils')
+const buildServer = require('./test-server')
+
+describe('Comments Websocket - routes', () => {
+  let server1 = null
+  let server2 = null
+  let client1 = null
+  let client2 = null
+
+  before(async () => {
+    await resetDb()
+    server1 = await buildServer({ host: '127.0.0.1', port: 8281, pluginOptions: { redis: { host: 'localhost', port: 6379 } } })
+    server2 = await buildServer({ host: '127.0.0.1', port: 8282, pluginOptions: { redis: { host: 'localhost', port: 6379 } } })
+    await server1.start()
+    await server2.start()
+  })
+
+  after(async () => {
+    await server1.stop()
+    await server2.stop()
+  })
+
+  describe('Websocket (2 servers) - adding a new comment', () => {
+    test('it should be pushed to resource subscribers', async (flags) => {
+      const resource = 'http://example.com'
+
+      client1 = new Nes.Client('ws://127.0.0.1:8281')
+      await client1.connect()
+
+      client2 = new Nes.Client('ws://127.0.0.1:8282')
+      await client2.connect()
+
+      await new Promise((resolve, reject) => {
+        const newComment = {
+          resource: resource,
+          reference: 'UUID',
+          content: 'MESSAGE',
+          author: 'AUTHOR'
+        }
+
+        function handler (event, flags) {
+          expect(event.comment).to.include(newComment)
+          expect(event.action).to.equal('add')
+
+          client1.disconnect()
+            .then(() => client2.disconnect())
+            .then(resolve)
+        }
+
+        return client1.subscribe(`/resources/${resource}`, handler)
+          .then(() => {
+            return client2.request({
+              method: 'POST',
+              path: '/comments',
+              payload: newComment
+            })
+          })
+      })
+    })
+  })
+})
