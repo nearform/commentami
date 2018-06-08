8d7ce7e5bc001ef7ebf201e7d9dd8612de99bf90
Shogun <shogun@cowtech.it>
2018-06-08 09:06:55 +0200

    chore(lint): Minor improvements.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,19 +7,20 @@ module.exports = {
     'prettier/standard'
   ],
   rules: {
+    'no-console': [1],
+    'max-len': [1, { code: 120, comments: 120 }],
     // This is inserted to make this compatible with prettier.
     curly: 0,
-    'no-console': ['warn'],
     /*
       This is inserted to make this compatible with prettier.
       Once https://github.com/prettier/prettier/issues/3845 and https://github.com/prettier/prettier/issues/3847 are solved this might be not needed any more.
     */
     'space-before-function-paren': 0,
-    // Remove once we introduce it
+    // Remove once we introduce then.
     'react/prop-types': 0,
-    // This is because usually double quotes are more common in HTML/JSX tags
+    // This is because usually double quotes are more common in HTML/JSX tags.
     'jsx-quotes': [2, 'prefer-double'],
-    // Let's make sure all JSX is clearly indicated by file extension
+    // Let's make sure all JSX is clearly indicated by file extension.
     'react/jsx-filename-extension': [2, { extensions: ['.jsx'] }]
   },
   globals: {
